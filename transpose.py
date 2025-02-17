25c410e88f62c380f0ce8cb4dee19c148cd37456
Todd <gyphonic2011@gmail.com>
2025-02-16 19:14:46 -0600

    V 1.1

--- transpose.py
+++ transpose.py
@@ -1,21 +1,27 @@
+#gyphonic/transpose.py
+#V1.1
+
 import pandas as pd
 import os
 
 def main():
     try:
         #boilerplate
+        print(" ")
+        print("gyphonic/transpose V1.1")
         print("Script to transpose a .csv file")
         print("Transposition swaps the rows and columns of a file.")
+        print(" ")
 
         #get current directory
         cwd = os.getcwd()
 
         #Get file
-        file = input("Path of file: >")
+        file = input("File: ")
 
         #verify
-        print(">" + file)
-        consent = input("Is this correct? Y/N")
+        print("> " + file)
+        consent = input("Is this correct? (Y/N) ")
 
         if consent.upper() == "Y":
             
@@ -24,20 +30,22 @@ def main():
                 df = pd.read_csv(file)
             except:
                 print("File error. Double check file type.")
-                break
 
-            #transpose
-            df = df.T
+            #drop first column index and transpose
+            try:
+                df = df.drop([0], axis = 1)
+                df = df.T
+            except:
+                print("Transposition error.")
 
             #save
             try:
                 fileT = "output.csv"
-                df.to_csv(fileT, index = False)
+                df.to_csv(fileT)
 
                 print("Completed. Saved to " + cwd + "/" + fileT)
             except:
                 print("Saving error.")
-                break
 
 
     except:
